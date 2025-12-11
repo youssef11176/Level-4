0bfc8204d6ea8fe6abe9f9ffcd01264bc4a8f52d
Retajalyy <Retaj.Ali.FCI22042@sadatacademy.edu.eg>
2025-12-11 18:31:25 +0200

    miss retaj commit

---
+++ session1.js
@@ -0,0 +1,6 @@
+const button=document.getElementById("myButton");
+const demo=document.getElementById("demo");
+
+button.addEventListener("click",function(){
+    demo.innerHTML="hello javascript";
+});
